b4af2b1d43d52b5f3a4cccaba011c260b464670b
Romaric <romaric.miaka.sio@gmail.com>
2023-07-18 11:55:38 +0200

    navigate through number

--- Program.cs
+++ Program.cs
@@ -63,3 +63,73 @@
 //     }
 // }
 
+// Part 2
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Main Menu:");
+            Console.WriteLine("1. Option 1");
+            Console.WriteLine("2. Option 2");
+            Console.WriteLine("3. Submenu");
+            Console.WriteLine("4. Exit");
+
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("You selected Option 1.");
+                    break;
+                case 2:
+                    Console.WriteLine("You selected Option 2.");
+                    break;
+                case 3:
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Submenu:");
+                        Console.WriteLine("1. Suboption 1");
+                        Console.WriteLine("2. Suboption 2");
+                        Console.WriteLine("3. Back");
+
+                        int subChoice = int.Parse(Console.ReadLine());
+
+                        switch (subChoice)
+                        {
+                            case 1:
+                                Console.WriteLine("You selected Suboption 1.");
+                                break;
+                            case 2:
+                                Console.WriteLine("You selected Suboption 2.");
+                                break;
+                            case 3:
+                                goto EndSubmenu;
+                            default:
+                                Console.WriteLine("Invalid choice.");
+                                break;
+                        }
+
+                        Console.Write("\nPress any key to continue...");
+                        Console.ReadKey(true);
+                    }
+
+                EndSubmenu:
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice.");
+                    break;
+            }
+
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
